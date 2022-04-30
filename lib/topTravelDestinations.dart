85b2d5082671ffc762d0e547e09b5b3488911189
Ali Asghar <aliasghar.zeeshan@gmail.com>
2022-04-30 19:41:48 +0500

    Top Travel Desinations done

--- lib/topTravelDestinations.dart
+++ lib/topTravelDestinations.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'searchPage.dart';
@@ -58,65 +59,156 @@ class topTravelDestinations extends StatelessWidget {
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      body: ListView.builder(
-          itemCount: TileList.length,
-          itemBuilder: (context, index) {
-            return Align(
-              alignment: Alignment.centerRight,
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width,
-                child: Card(
-                  // color: Colors.black,
-                  elevation: 0,
-                  margin: EdgeInsets.fromLTRB(0, 5, 0, 5),
-                  child: ListTile(
-                    title: ClipRRect(
-                      borderRadius: BorderRadius.circular(5.0),
-                      child: Image.network(TileList[index].url),
-                    ),
-                    subtitle: Column(
-                      children: [
-                        Row(
+      // body: ListView.builder(
+      //     itemCount: TileList.length,
+      //     itemBuilder: (context, index) {
+      //       return Align(
+      //         alignment: Alignment.centerRight,
+      //         child: SizedBox(
+      //           width: MediaQuery.of(context).size.width,
+      //           child: Card(
+      //             // color: Colors.black,
+      //             elevation: 0,
+      //             margin: EdgeInsets.fromLTRB(0, 5, 0, 5),
+      //             child: ListTile(
+      //               title: ClipRRect(
+      //                 borderRadius: BorderRadius.circular(5.0),
+      //                 child: Image.network(TileList[index].url),
+      //               ),
+      //               subtitle: Column(
+      //                 children: [
+      //                   Row(
+      //                     children: [
+      //                       Text(
+      //                         TileList[index].name,
+      //                         style: TextStyle(
+      //                             fontSize: 20,
+      //                             fontWeight: FontWeight.bold,
+      //                             color: Colors.black),
+      //                       ),
+      //                     ],
+      //                   ),
+      //                   Container(
+      //                     width: double.infinity,
+      //                     child: Text(
+      //                       TileList[index].days,
+      //                       style: TextStyle(fontSize: 20, color: Colors.black),
+      //                       textAlign: TextAlign.left,
+      //                     ),
+      //                   ),
+      //                   Row(
+      //                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //                     children: [
+      //                       Text(
+      //                         TileList[index].company,
+      //                         style:
+      //                             TextStyle(fontSize: 20, color: Colors.black),
+      //                       ),
+      //                       Text(
+      //                         TileList[index].price,
+      //                         style:
+      //                             TextStyle(fontSize: 20, color: Colors.black),
+      //                       ),
+      //                     ],
+      //                   )
+      //                 ],
+      //               ),
+      //             ),
+      //           ),
+      //         ),
+      //       );
+      //     }),
+       body: topPackages(),
+    );
+  }
+}
+
+class topPackages extends StatefulWidget {
+  @override
+  _topPackagesState createState() => _topPackagesState();
+}
+
+class _topPackagesState extends State<topPackages> {
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference Packages =
+        FirebaseFirestore.instance.collection('Packages');
+    return StreamBuilder(
+      stream: Packages.orderBy('sales').snapshots(),
+      builder: (context, AsyncSnapshot snapshot) {
+        if (snapshot.hasData && snapshot.data.docs.length > 0) {
+          print(snapshot.data.docs.length);
+          return ListView.builder(
+              itemCount: snapshot.data.docs.length,
+              itemBuilder: (context, index) {
+                return Align(
+                  alignment: Alignment.centerRight,
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: Card(
+                      // color: Colors.black,
+                      elevation: 0,
+                      margin: EdgeInsets.fromLTRB(0, 5, 0, 5),
+                      child: ListTile(
+                        title: ClipRRect(
+                          borderRadius: BorderRadius.circular(5.0),
+                          child: Image.network(
+                              snapshot.data.docs[index]['photoUrl']),
+                        ),
+                        subtitle: Column(
                           children: [
-                            Text(
-                              TileList[index].name,
-                              style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black),
+                            Row(
+                              children: [
+                                Text(
+                                  'Package ' +
+                                      snapshot.data.docs[index]['Package name'],
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black),
+                                ),
+                              ],
                             ),
+                            Container(
+                              width: double.infinity,
+                              child: Text(
+                                snapshot.data.docs[index]['days'] + ' Days',
+                                style: TextStyle(
+                                    fontSize: 16, color: Colors.black),
+                                textAlign: TextAlign.left,
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  snapshot.data.docs[index]['Agency Name'],
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.black),
+                                ),
+                                Text(
+                                  '\$ ' + snapshot.data.docs[index]['price'],
+                                  style: TextStyle(
+                                      fontSize: 20, color: Colors.black),
+                                ),
+                              ],
+                            )
                           ],
                         ),
-                        Container(
-                          width: double.infinity,
-                          child: Text(
-                            TileList[index].days,
-                            style: TextStyle(fontSize: 20, color: Colors.black),
-                            textAlign: TextAlign.left,
-                          ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              TileList[index].company,
-                              style:
-                                  TextStyle(fontSize: 20, color: Colors.black),
-                            ),
-                            Text(
-                              TileList[index].price,
-                              style:
-                                  TextStyle(fontSize: 20, color: Colors.black),
-                            ),
-                          ],
-                        )
-                      ],
+                      ),
                     ),
                   ),
-                ),
-              ),
-            );
-          }),
+                );
+              });
+        } else {
+          return (Center(
+            child: Text(
+              'Not Found',
+              style: TextStyle(fontSize: 25),
+            ),
+          ));
+        }
+      },
     );
   }
 }
